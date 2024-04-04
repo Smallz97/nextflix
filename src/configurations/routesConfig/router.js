4e032c5fc8d7ac41fcec3928da6f458e48a9d13f
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-04-04 22:09:30 +0100

    updated the application's authentication logic

--- src/configurations/routesConfig/router.js
+++ src/configurations/routesConfig/router.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import SignUp from "../../pages/auth/SignUp";
+// import SignUp from "../../pages/auth/SignUp";
+import Login from "../../pages/auth/Login";
 import HomePage from "../../pages/app/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: SignUp,
+    Component: Login,
     // ErrorBoundary: ErrorBoundary,
     // children: childrenroutes,
   },
