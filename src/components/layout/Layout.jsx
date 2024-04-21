725c67484a9533ccf767599486f4b6f882bc43af
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-04-21 01:55:12 +0100

    implemented active states for the navigation links

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,28 @@
 import { useSelector } from 'react-redux'
+import NavigationMenu from '../navigation/Navigation'
+import SearchBar from '../search/Search';
 import { Outlet, Navigate } from "react-router-dom";
+import styles from './Layout.module.css'
 import { Component } from "react";
 
 export function Layout() {
     const { authenticated } = useSelector((state) => state.authentication);
     return (
         <>
-            {authenticated ? <Outlet /> : <Navigate to="/" />}
+            {authenticated ?
+                <div className={styles.layout}>
+                    <div className={styles.navigationBar}>
+                        <NavigationMenu />
+                    </div>
+                    <div className={styles.searchBar}>
+                        <SearchBar />
+                    </div>
+                    <div className={styles.outlet}>
+                        <Outlet />
+                    </div>
+                </div>
+                : <Navigate to="/" />
+            }
         </>
     );
 }
