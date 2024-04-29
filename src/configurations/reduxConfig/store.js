f97af23e7df526761404f41f2a0c02773d9d5b97
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-04-29 05:57:55 +0100

    included a local JSON file containing list of movies, a thumbnails folder and refactored the code to render the  movies dynamically

--- src/configurations/reduxConfig/store.js
+++ src/configurations/reduxConfig/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import movieListReducer from "./movieSlice";
 import authenticationReducer from "./authenticationSlice";
 
 export const store = configureStore({
   reducer: {
     authentication: authenticationReducer,
+    moviesList: movieListReducer,
   },
 });
