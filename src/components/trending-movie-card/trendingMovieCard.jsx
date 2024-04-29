eb2cccd35a7cb51ea4088ad1f86d9baefa7e4ee9
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-04-29 20:52:30 +0100

    included a button for playing videos that displays when you hover over a video

--- src/components/trending-movie-card/trendingMovieCard.jsx
+++ src/components/trending-movie-card/trendingMovieCard.jsx
@@ -1,10 +1,10 @@
-import { Bookmark, Bookmarked } from "../../assets/icons/Icons";
+import { PlayIcon, SeriesIcon, MoviesIcon, Bookmark, Bookmarked } from "../../assets/icons/Icons";
 import styles from './trendingMovieCard.module.css'
 
 const TrendingMovieCard = ({ movie, thumbnail }) => {
     const { small, large } = thumbnail;
     return (
-        <div className={styles.trendingMovieCard}>
+        <>
             <div className={styles.trendingItem}>
                 <picture className={styles.thumbnail}>
                     <source srcSet={`/${small}`} media="(max-width: 39rem)" />
@@ -16,8 +16,35 @@ const TrendingMovieCard = ({ movie, thumbnail }) => {
                         {movie.isBookmarked ? <Bookmarked /> : <Bookmark />}
                     </div>
                 </div>
+                <div className={styles.playIcon}>
+                    <div className={styles.play}>
+                        <PlayIcon />
+                    </div>
+                    <div className={styles.text}>Play</div>
+                </div>
+                <div className={styles.movieDetailsWrap}>
+                    <div className={styles.movieDetails}>
+                        <div className={styles.movieYear}>
+                            {movie.year}
+                        </div>
+                        <div className={styles.circle}></div>
+                        <div className={styles.iconWrap}>
+                            <div className={styles.icon}>
+                                {movie.category === "Movie" ? <MoviesIcon fillColor={"#FFFFFF"} /> : <SeriesIcon fillColor={"#FFFFFF"} />}
+                            </div>
+                            {movie.category}
+                        </div>
+                        <div className={styles.circle}></div>
+                        <div className={styles.rating}>
+                            {movie.rating}
+                        </div>
+                    </div>
+                    <div className={styles.movieTitle}>
+                        {movie.title}
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
